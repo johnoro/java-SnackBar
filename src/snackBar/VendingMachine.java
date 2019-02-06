7a260bd970b098127f688382451343a9f1981053
johnoro <officialjohnorourke@gmail.com>
2019-02-06 18:38:04 -0500

    Add VendingMachine class

---
+++ src/snackBar/VendingMachine.java
@@ -0,0 +1,23 @@
+package snackBar;
+
+public class VendingMachine {
+  private static int currentId = 0;
+  private int id;
+  private String name;
+
+  public void VendingMachine(String name) {
+    this.name = name;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
