3219bb6bcfc3cae779a78c81889d238b35eaeea2
johnoro <officialjohnorourke@gmail.com>
2019-02-06 23:47:16 -0500

    Add processing for main
    I'm still going to reorganize it a bit

--- src/snackBar/Main.java
+++ src/snackBar/Main.java
@@ -1,6 +1,14 @@
 package snackBar;
 
 public class Main {
+  public static void buy(Customer customer, Snack snack, int amount) {
+    customer.buy(snack.getTotal(amount));
+    snack.buy(amount);
+
+    System.out.println(customer.getName() + "'s cash on hand: " + customer.getCash() + "\n" +
+                      "Quantity of " + snack.getName() + ": " + snack.getQuantity());
+  }
+
   private static void instantiateAndProcess() {
     Customer jane = new Customer("Jane", 45.25);
     Customer bob = new Customer("Bob", 33.14);
@@ -16,7 +24,19 @@ public class Main {
     Snack soda = new Snack("soda", 24, 2.50, drink.getId());
     Snack water = new Snack("water", 20, 2.75, drink.getId());
 
-    // processing
+    buy(jane, soda, 3);
+    buy(jane, pretzel, 1);
+    buy(bob, soda, 2);
+
+    jane.addCash(10);
+    System.out.println("Cash on hand: " + jane.getCash());
+
+    buy(jane, chocolateBar, 1);
+
+    pretzel.addQuantity(12);
+    System.out.println("Quantity of " + pretzel.getName() + ": " + pretzel.getQuantity());
+
+    buy(bob, pretzel, 3);
   }
 
   public static void main(String[] args) {
